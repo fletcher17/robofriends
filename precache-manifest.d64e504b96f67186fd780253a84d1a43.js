831c9135974f63ebd05da53bc704c38aa08b2a80
fletcher17 <ororo17@gmail.com>
2019-11-04 23:10:11 +0100

    Updates

---
+++ precache-manifest.d64e504b96f67186fd780253a84d1a43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "395caf81fc7fa6069621d23324ca2550",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b407be9e33b14d693021",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "819ae3903d048d0dee07",
+    "url": "/static/css/main.0102cd3a.chunk.css"
+  },
+  {
+    "revision": "b407be9e33b14d693021",
+    "url": "/static/js/2.1b44efa3.chunk.js"
+  },
+  {
+    "revision": "819ae3903d048d0dee07",
+    "url": "/static/js/main.b419830b.chunk.js"
+  },
+  {
+    "revision": "244a77a7bb4101583953",
+    "url": "/static/js/runtime~main.9e8d0c72.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
